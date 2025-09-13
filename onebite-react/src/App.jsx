5fd35d2a7bdd3da7484e1073ae1fe4521a63f5fd
이수빈 <supotato@henery.xyz>
2025-09-13 14:37:12 +0900

    style: counter app UI

--- onebite-react/src/App.jsx
+++ onebite-react/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer/>
+      </section>
+      <section>
+        <Controller/>
+      </section>
+    </div>
   )
 }
 
---
+++ onebite-react/src/components/Viewer.jsx
@@ -0,0 +1,8 @@
+const Viewer = () => {
+    return <div>
+        <div>현재 카운트 :</div>
+        <h1>0</h1>
+    </div>
+}
+
+export default Viewer;
