1ca4bff92978c088b764bbe154b22099236cf5f7
이수빈 <supotato@henery.xyz>
2025-09-13 15:09:59 +0900

    feat: useEffect 사용

--- onebite-react/src/App.jsx
+++ onebite-react/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    console.log(`count : ${count} / input: ${input}`);
+  }, [count, input]);
+  // 이때, count는 "의존성 배열"이다.
+  // dependency array
+  // deps
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -13,6 +21,11 @@ function App() {
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e) => {
+          setInput(e.target.value);
+        }}/>
+      </section>
       <section>
         <Viewer count={count}/>
       </section>
