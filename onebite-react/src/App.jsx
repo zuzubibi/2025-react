ec5c0d70ed8c2b53b6b71d51c58845ee105cda1e
이수빈 <supotato@henery.xyz>
2025-09-13 15:21:08 +0900

    feat: useEffect로 라이프사이클 제어하기

--- onebite-react/src/App.jsx
+++ onebite-react/src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState, useEffect } from 'react'
+import Even from './components/Even'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState('');
 
+  const isMount = useRef(false);
+
+  // 1. 마운트: 탄생
   useEffect(() => {
-    console.log(`count : ${count} / input: ${input}`);
-  }, [count, input]);
-  // 이때, count는 "의존성 배열"이다.
-  // dependency array
-  // deps
+    console.log('mount');
+  }, []);
+
+  // 2. 업테이트: 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log('update');
+  });
+
+  // 3. 언마운트: 죽음 -> Even.jsx 확인
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -28,6 +40,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count}/>
+        {count % 2 === 0 ? <Even/> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton}/>
