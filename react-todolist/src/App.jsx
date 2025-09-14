5953450b3dbcc9ad8effc2a900e9cff7faa95ca1
이수빈 <supotato@henery.xyz>
2025-09-14 15:58:26 +0900

    feat: todo list create, read, update, delete

--- react-todolist/src/App.jsx
+++ react-todolist/src/App.jsx
@@ -1,16 +1,61 @@
-import { useState } from 'react'
 import './App.css'
+import { useState, useRef } from 'react'
 import Header from './components/Header'
 import List from './components/List'
 import Editor from './components/Editor'
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    date: new Date().getTime(),
+  },
+]
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    }
+    setTodos([newTodo, ...todos])
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(todos.map((todo) => todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo));
+  }
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  }
 
   return (
     <div className="App">
-      <Header></Header>
-      <Editor></Editor>
-      <List></List>
+      <Header/>
+      <Editor onCreate={onCreate}/>
+      <List 
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
     </div>
   )
 }
--- react-todolist/src/components/List.jsx
+++ react-todolist/src/components/List.jsx
@@ -1,15 +1,47 @@
 import './List.css';
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos, onUpdate, onDelete }) => {
+
+    const [search, setSearch] = useState('');
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const getFilteredData = () => {
+        if (search === '') {
+            return todos;
+        }
+        return todos.filter((todo) => 
+            todo.content
+            .toLowerCase()
+            .includes(search.toLowerCase())
+        );
+    };
+
+    const filteredTodos = getFilteredData();
+
     return (
         <div className="List">
             <h4>Todo List 🍀</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <input 
+                value={search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요"
+            />
             <div className="todos_wrapper">
-                <TodoItem></TodoItem>
-                <TodoItem></TodoItem>
-                <TodoItem></TodoItem>
+                {filteredTodos.map((todo) => {
+                    return(
+                        <TodoItem 
+                            key={todo.id}
+                            {...todo}
+                            onUpdate={onUpdate}
+                            onDelete={onDelete}
+                        />
+                    ) 
+                })}
             </div>
         </div>
     )
