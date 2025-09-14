d1f2fea25b7351a446af216b93b077b8d0f360f2
이수빈 <supotato@henery.xyz>
2025-09-14 16:30:07 +0900

    feat: 기존 기능 useReducer로 리팩토링

--- react-todolist/src/App.jsx
+++ react-todolist/src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
-import { useState, useRef } from 'react'
+import { useState, useRef, useReducer } from 'react'
 import Header from './components/Header'
 import List from './components/List'
 import Editor from './components/Editor'
-import Exam from './components/Exam'
 
 const mockData = [
   {
@@ -26,38 +25,62 @@ const mockData = [
   },
 ]
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE': 
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId 
+          ? {...item, isDone: !item.isDone} 
+          : item
+      );
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    }
-    setTodos([newTodo, ...todos])
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo) => todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo));
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    })
   }
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    })
   }
 
   return (
     <div className="App">
-      <Exam/>
-      {/* <Header/>
+      <Header/>
       <Editor onCreate={onCreate}/>
       <List 
         todos={todos}
         onUpdate={onUpdate}
         onDelete={onDelete}
-      /> */}
+      />
     </div>
   )
 }
