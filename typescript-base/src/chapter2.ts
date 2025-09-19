3335cf4de6c76aa315547b3a2ecbf102c1a0fba0
이수빈 <supotato@henery.xyz>
2025-09-19 22:09:13 +0900

    chapter2) 배열과 튜플

---
+++ typescript-base/src/chapter2.ts
@@ -0,0 +1,29 @@
+// 배열
+let numArr: number[] = [1,2,3];
+
+let strArr = ["hello", "im", "winterlood"];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+// 배열에 들어가는 요소들의 타입이 다양할 경우
+let multiArr: (number | string)[] = [1, "hello"];
+
+
+// 다차원 배열의 타입을 정의하는 방법
+let deobleArr: number[][] = [
+    [1,2,3],
+    [4,5],
+];
+
+// 튜플
+// 길이와 타입이 고정된 배열
+let tup1: [number, number] = [1,2];
+let tup2: [number, string, boolean] = [1, "hello", true];
+
+const user: [string, number][] = [
+    ['이수빈', 1],
+    ['장아무개', 5],
+    ['김아무개', 3],
+    ['최아무개', 4],
+    ['홍길동', 2],
+]
