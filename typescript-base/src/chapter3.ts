ddc6941b75d6f4dc501cf33114ef07876e34275f
이수빈 <supotato@henery.xyz>
2025-09-19 22:17:22 +0900

    chapter3) 객체

---
+++ typescript-base/src/chapter3.ts
@@ -0,0 +1,16 @@
+// object
+let user: {
+    id?: number;
+    name: string;
+} = {
+    id: 1,
+    name: '이수빈',
+}
+
+let config: {
+    readonly apiKey: string,
+} = {
+    apiKey: "My API KEY",
+};
+
+// config.apiKey = 'hacked';
