8a9d865f429bc4610de4f15e8a9165bcaab1734d
이수빈 <supotato@henery.xyz>
2025-09-21 15:54:18 +0900

    chapter 7) void와 never 타입

---
+++ typescript-base/src/chapter7.ts
@@ -0,0 +1,18 @@
+// void
+// void -> 공허 -> 아무것도 없다.
+// void -> 아무것도 없음을 의미하는 타입
+function func1(): string {
+    return "hello";
+}
+
+function func2(): void {
+    console.log("hello");
+}
+
+// never
+// never -> 존재하지 않는
+// 불가능한 타입
+
+function func3(): void {
+    while (true) {}
+}
