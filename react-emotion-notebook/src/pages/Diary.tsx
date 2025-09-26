7308724fe4073003475ba7beaaf5bae52db5dbc9
이수빈 <supotato@henery.xyz>
2025-09-26 11:28:57 +0900

    [feat] 동적경로 - url parameter

--- react-emotion-notebook/src/pages/Diary.tsx
+++ react-emotion-notebook/src/pages/Diary.tsx
@@ -1,5 +1,9 @@
+import { useParams } from 'react-router-dom';
+
 const Diary = () => {
-    return <div>Diary</div>
+    const param = useParams();
+    console.log(param);
+    return <div>{param.id}번 일기입니다 ~~</div>
 }
 
 export default Diary;
