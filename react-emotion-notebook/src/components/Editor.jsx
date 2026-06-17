7ab2ba565ca331e8fca9249c30c606322b41f50a
zuzubibi <lsb9938@naver.com>
2026-06-17 19:01:27 +0900

    Feat: 새일기 쓰기 기능 ㄹ추가

--- react-emotion-notebook/src/components/Editor.jsx
+++ react-emotion-notebook/src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import './Editor.css';
 import EmotionItem from './EmotionItem';
 import Button from './Button';
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom';
 
 const emotionList = [
     { emotionId: 1, emotionName: '완전 좋음' },
@@ -10,34 +12,94 @@ const emotionList = [
     { emotionId: 5, emotionName: '끔찍함' },
 ];
 
-const Editor = () => {
-    const emotionId = 3;
+
+const getStringDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+
+    return `${year}-${month}-${date}`;
+};
+    
+const Editor = ({ onSubmit }) => {
+    const nav = useNavigate();
+    const [input, setInput] = useState({
+        createdDate: new Date(),
+        emotionId: 3,
+        content: "",
+    });
+
+    const onChangeInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        console.log("옹?", name, value);
+        if (name === 'createdDate') {
+            value = new Date(value);
+        }
+
+        setInput({
+            ...input,
+            [name]: value,
+        })
+    }
+
+    const onClickSubmitButton = () => {
+        console.log('오???', input);
+        onSubmit(input);
+    };
 
     return (
       <div className="Editor">
         <section className="date_section">
             <h4>오늘의 날짜</h4>
-            <input type="date" />
+            <input
+                name="createdDate"
+                onChange={onChangeInput}
+                value={getStringDate(input.createdDate)}
+                type="date"
+            />
         </section>
         <section className="emotion_section">
-            <h4>오늘의 감정</h4>    
-            <div className='emotion_list_wrapper'>
-                {emotionList.map((item) => (
-                    <EmotionItem
-                        key={item.emotionId}
-                        {...item}
-                        isSelected={item.emotionId === emotionId}
-                    />
-                ))}
-            </div>    
+          <h4>오늘의 감정</h4>
+          <div className="emotion_list_wrapper">
+            {emotionList.map((item) => (
+              <EmotionItem
+                onClick={() =>
+                    onChangeInput({
+                        target: {
+                            name: "emotionId",
+                            value: item.emotionId
+                        },
+                    })
+                }
+                key={item.emotionId}
+                {...item}
+                isSelected={item.emotionId === input.emotionId}
+              />
+            ))}
+          </div>
         </section>
+
         <section className="content_section">
-            <h4>오늘의 일기</h4>    
-            <textarea placeholder='오늘은 어땠나요?'></textarea>
+            <h4>오늘의 일기</h4>
+            <textarea
+                name="content"
+                value={input.content}
+                onChange={onChangeInput}
+                placeholder="오늘은 어땠나요?"
+            />
         </section>
         <section className="button_section">
-            <Button text={'취소하기'} />
-                <Button text={'작성완료'} type={"POSITIVE"} />
+            <Button onClick={ () => nav(-1)} text={"취소하기"} />
+            <Button onClick={onClickSubmitButton} text={"작성완료"} type={"POSITIVE"} />
         </section>
       </div>
     );
--- react-emotion-notebook/src/components/EmotionItem.jsx
+++ react-emotion-notebook/src/components/EmotionItem.jsx
@@ -1,9 +1,16 @@
 import './EmotionItem.css';
 import getEmotionImage from "../util/get-emotion-image";
 
-const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+const EmotionItem = ({
+    emotionId,
+    emotionName,
+    isSelected,
+    onClick,
+}) => {
     return (
-        <div className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+        <div
+            onClick={onClick}
+            className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
             <img className='emotion_img' src={getEmotionImage(emotionId)} />
             <div className='emotion_name'>{emotionName}</div>
         </div>
